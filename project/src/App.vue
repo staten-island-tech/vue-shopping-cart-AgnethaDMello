7c42139ea70bef06e4325ad62f983e66b5e5eedd
AgnethaDMello <agnethad@nycstudents.net>
2022-02-13 21:28:41 -0500

    hHHHH

--- project/src/App.vue
+++ project/src/App.vue
@@ -1,10 +1,5 @@
 <template>
   <div id="app">
-    <h1 id="header">Framblees!</h1>
-    <div id="frankie">
-      <h2>These are all made by my friend Frankie! Visit his shop!</h2>
-      <a href="https://www.etsy.com/shop/FrambleesArt">Etsy Shop</a>
-    </div>
     <div id="nav">
       <router-link to="/">Items</router-link> |
       <router-link to="/about">Your Cart</router-link>
@@ -14,34 +9,20 @@
 </template>
 
 <style>
-body {
-  background-color: rgb(197, 235, 235);
-}
 #app {
-  font-family: Comic Sans MS, Comic Sans, cursive;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
+  color: #2c3e50;
 }
 
-h1 {
-  text-align: center;
-  font-size: 5rem;
-  color: rgb(20, 69, 150);
-  padding: 3rem;
-  background-color: darkseagreen;
-  margin-top: 0rem;
-  border: solid 1rem rgb(61, 102, 97);
-}
-
-#frankie {
-  color: rgb(17, 17, 109);
-  margin-bottom: 1rem;
-  font-size: 1rem;
+p {
+  color: blueviolet;
 }
 
 #nav {
-  padding: 0rem;
+  padding: 30px;
 }
 
 #nav a {
--- project/src/components/Card.vue
+++ project/src/components/Card.vue
@@ -2,9 +2,7 @@
   <div>
     <h2>{{ title }}</h2>
     <img v-bind:src="image" class="image" alt="" />
-    <p>${{ price }}</p>
-    <p>{{ index }}</p>
-    <button>Add to Cart!</button>
+    <p>{{ description }}</p>
   </div>
 </template>
 
@@ -13,7 +11,7 @@ export default {
   name: "Card",
   props: {
     title: String,
-    price: Number,
+    description: String,
     image: String,
   },
 };
